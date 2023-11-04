7a59fd0ea320bc8397b4102ede29bbd8e4363dee
fai-sas <craftedbyfaisal@gmail.com>
2023-11-04 19:58:23 +0600

    hook crated to get all rooms using tanstack query

---
+++ src/Hooks/useGetRooms.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import { useQuery } from '@tanstack/react-query'
+
+const useGetRooms = () => {
+  const { data, isLoading, isFetching, refetch } = useQuery({
+    queryKey: ['getRooms'],
+    queryFn: async () => {
+      const data = await fetch('http://localhost:5000/api/v1/get-rooms')
+      return await data.json()
+    },
+  })
+  return { data, isLoading, isFetching, refetch }
+}
+export default useGetRooms
