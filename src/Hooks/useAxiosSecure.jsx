f8313682360e067e565cc91207eaa70f648931cd
fai-sas <craftedbyfaisal@gmail.com>
2023-11-05 21:26:40 +0600

    axios interceptors created

---
+++ src/Hooks/useAxiosSecure.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable no-unused-vars */
+import axios from 'axios'
+import useAuth from './useAuth'
+import { useNavigate } from 'react-router-dom'
+
+const instance = axios.create({
+  baseURL: 'http://localhost:5000/api/v1',
+  withCredentials: true,
+})
+
+const useAxiosSecure = () => {
+  const { logOut } = useAuth()
+  const navigate = useNavigate()
+
+  instance.interceptors.response.use(
+    function (response) {
+      return response
+    },
+    function (error) {
+      if (error.response.status === 401 || error.response.status === 403) {
+        logOut()
+        navigate('/login')
+      }
+    }
+  )
+  return instance
+}
+export default useAxiosSecure
