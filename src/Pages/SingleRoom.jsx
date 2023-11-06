4fea8f825834d1a4f6b20a86943ca5761ebdbd55
fai-sas <craftedbyfaisal@gmail.com>
2023-11-06 23:00:04 +0600

    room details page styles added

--- src/Pages/SingleRoom.jsx
+++ src/Pages/SingleRoom.jsx
@@ -8,6 +8,7 @@ const SingleRoom = () => {
   const roomData = useLoaderData()
 
   const {
+    name,
     _id,
     description,
     price_per_night,
@@ -15,32 +16,97 @@ const SingleRoom = () => {
     availability,
     images,
     special_offers,
+    image1,
+    image2,
+    image3,
+    reviews,
   } = roomData
 
   return (
-    <div>
-      <h1>{_id}</h1>
-      <h1>{description}</h1>
-      <h1>{price_per_night}</h1>
-      <h1>{room_size}</h1>
-      <h1>{availability}</h1>
-      <h1>{special_offers}</h1>
-      <div className='grid grid-cols-3 gap-2 mx-16'>
-        {images.map((image, index) => (
+    <section className='container p-8 mx-auto'>
+      <h1 className='my-8 text-2xl font-black text-transparent lg:text-5xl bg-clip-text bg-gradient-to-r from-indigo-500 to-teal-500'>
+        {name}
+      </h1>
+
+      <header
+        className='relative block p-6 mx-auto my-8 text-white bg-gray-900 border border-gray-100 rounded-lg'
+        href='#'
+      >
+        <div className='flex flex-wrap justify-around gap-4'>
+          <h2>Available: {availability}</h2>
+          <p>Price: ${price_per_night}</p>
+          <p>Size: {room_size}</p>
+          {/* <p>Reviews: {reviews ? reviews : 'No Reviews Yet!'}</p> */}
+          <p>
+            Special Offer:{' '}
+            {special_offers ? [...special_offers] : 'Check Later for Offers!'}
+          </p>
+        </div>
+        <span className='absolute inset-x-0 bottom-0 h-2 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600'></span>
+      </header>
+
+      <div className='grid grid-cols-2 gap-4 my-8 sm:grid-cols-3 md:gap-6 xl:gap-8'>
+        <article className='relative flex items-end h-48 overflow-hidden bg-gray-100 rounded-lg shadow-lg group md:col-span-3 md:h-80'>
           <img
-            key={index}
-            src={image}
-            className='object-cover w-48 h-48'
-            alt={`Room ${description} Image`}
+            src={image1}
+            loading='lazy'
+            alt='Photo by Martin Sanchez'
+            className='absolute inset-0 object-cover object-center w-full h-full transition duration-200 group-hover:scale-110'
           />
-        ))}
+
+          <div className='absolute inset-0 opacity-50 pointer-events-none bg-gradient-to-t from-gray-800 via-transparent to-transparent'></div>
+        </article>
+
+        <article className='relative flex items-end h-48 overflow-hidden bg-gray-100 rounded-lg shadow-lg group md:col-span-2 md:h-80'>
+          <img
+            src={image2}
+            loading='lazy'
+            alt='Photo by Martin Sanchez'
+            className='absolute inset-0 object-cover object-center w-full h-full transition duration-200 group-hover:scale-110'
+          />
+
+          <div className='absolute inset-0 opacity-50 pointer-events-none bg-gradient-to-t from-gray-800 via-transparent to-transparent'></div>
+        </article>
+
+        <article className='relative flex items-end h-48 overflow-hidden bg-gray-100 rounded-lg shadow-lg group md:h-80'>
+          <img
+            src={image3}
+            loading='lazy'
+            alt='Photo by Lorenzo Herrera'
+            className='absolute inset-0 object-cover object-center w-full h-full transition duration-200 group-hover:scale-110'
+          />
+
+          <div className='absolute inset-0 opacity-50 pointer-events-none bg-gradient-to-t from-gray-800 via-transparent to-transparent'></div>
+        </article>
       </div>
-      <Link to={`/bookRoom/${_id}`}>
-        <button className='block p-2 mx-auto my-8 bg-green-400 rounded-xl'>
-          Book Now
-        </button>
-      </Link>
-    </div>
+
+      {/*  */}
+
+      <article className='py-20 my-20 text-white bg-gray-900 rounded-md'>
+        <div className='max-w-6xl px-4 mx-auto sm:px-6 lg:px-8'>
+          <div className='flex flex-col items-center justify-between lg:flex-row'>
+            <div className=' lg:w-3/4'>
+              <p className='mb-4 text-2xl font-semibold leading-relaxed '>
+                {description}
+              </p>
+            </div>
+
+            {/*  */}
+            <div className='relative inline-flex '>
+              <div className='absolute transition-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt'></div>
+              <Link
+                to={`/bookRoom/${_id}`}
+                className='relative inline-flex items-center justify-center px-8 py-4 text-lg font-bold text-white transition-all duration-200 bg-gray-900 font-pj rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900'
+                role='button'
+              >
+                Book Now
+              </Link>
+            </div>
+            {/*  */}
+          </div>
+        </div>
+      </article>
+    </section>
   )
 }
 export default SingleRoom
